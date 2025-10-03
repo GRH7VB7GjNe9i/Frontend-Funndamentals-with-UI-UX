8a5a21f436a6c14c6d05238492d2348d4d284ff0
Rushikesh khandu khalkar <143065547+GRH7VB7GjNe9i@users.noreply.github.com>
2025-10-04 00:27:12 +0530

    Add files via upload

---
+++ gsap/script.js
@@ -0,0 +1,16 @@
+gsap.from("#nav h2",{
+  y:-50,
+  delay:0.5,
+  duration:0.8,
+  opacity:0,
+  stagger:0.2
+})
+gsap.from("#main h1",{
+  x:-500,
+  delay:0.5,
+  duration:0.8,
+  opacity:0,
+  stagger:0.2
+})
+
+
